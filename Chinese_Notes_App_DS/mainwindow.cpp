27b39c01754a1b9d4e6e7483d22fb969bd666e5b
PauloWgDev <paulowaimberg@gmail.com>
2023-05-13 15:54:04 -0400

    new file:   Chinese_Notes_App_DS/.gitignore 	new file:   Chinese_Notes_App_DS/Chinese_Notes_App_DS.pro 	new file:   Chinese_Notes_App_DS/main.cpp 	new file:   Chinese_Notes_App_DS/mainwindow.cpp 	new file:   Chinese_Notes_App_DS/mainwindow.h 	new file:   Chinese_Notes_App_DS/mainwindow.ui

---
+++ Chinese_Notes_App_DS/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+int var = 0;
+
+void MainWindow::on_pushButton_clicked()
+{
+    var = 1;
+}
+
